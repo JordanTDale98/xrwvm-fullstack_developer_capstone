e7401077d964f7ea46fd87db52798a5f49053d98
jordantdale98 <jordantdale98@gmail.com>
2024-05-15 11:45:36 -0400

    Lint round 3

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -63,8 +63,8 @@ def registration(request):
         logger.debug("{} is a new user.".format(username))
 
     if not username_exist:
-        user = User.objects.create(username=username, first_name=first_name, 
-        last_name=last_name, password=password, email=email)
+        user = User.objects.create(username=username, first_name=first_name,
+            last_name=last_name, password=password, email=email)
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
@@ -82,13 +82,13 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, 
-        "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name,
+            "CarMake": car_model.car_make.name})
 
     return JsonResponse({"CarModels": cars})
 
 
-# Update the `get_dealerships` render list of dealerships all by default, 
+# Update the `get_dealerships` render list of dealerships all by default,
 # particular state if state is passed
 def get_dealerships(request, state="All"):
     if (state == "All"):
