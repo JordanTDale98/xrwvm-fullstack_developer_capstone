a2a36185f453abb0a8ed39d3c67c20b6e6b0e794
jordantdale98 <jordantdale98@gmail.com>
2024-05-15 11:20:38 -0400

    syntax fixes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,20 +1,16 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Car Make model
-
 class CarMake(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
 
     def __str__(self):
         return self.name
-
-
 
 # Car Model model
 class CarModel(models.Model):
