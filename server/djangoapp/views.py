0ca7797689dcf45e51e5c6921032907170e68599
jordantdale98 <jordatndale98@gmail.com>
2024-05-16 19:10:50 -0400

    Pylint Round 4

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -63,8 +63,10 @@ def registration(request):
         logger.debug("{} is a new user.".format(username))
 
     if not username_exist:
-        user = User.objects.create(username=username, first_name=first_name,
-            last_name=last_name, password=password, email=email)
+        user = User.objects.create(
+            username=username, first_name=first_name, 
+            last_name=last_name, password=password, email=email
+            )
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
@@ -82,8 +84,10 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name,
-            "CarMake": car_model.car_make.name})
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+            })
 
     return JsonResponse({"CarModels": cars})
 
@@ -125,12 +129,12 @@ def get_dealer_details(request, dealer_id):
 # Create a `add_review` view to submit a review
 def add_review(request):
     if request.user.is_anonymous is False:
-        data = json.loads(request.body)
         try:
             return JsonResponse({"status": 200})
         except Exception as err:
             print(f"Unexpected {err=}, {type(err)=}")
-            return JsonResponse({"status": 401, "message": 
-            "Error in posting review"})
+            return JsonResponse({
+                "status": 401, "message": "Error in posting review"
+                })
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
